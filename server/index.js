ab95a752d0dad30dd839b86550c93fda23faebfc
dima- <dima-tkachenko@mail.ru>
2021-03-31 02:54:42 +0300

    add registration

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const authRouter = require("./routes/auth.routes")
 const app = express()
 const PORT = config.get('serverPort')
 
 app.use(express.json())
+app.use("/api/auth", authRouter)
 app.use(express.static('static'))
 
 const start = async () => {
