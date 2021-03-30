629c0bdbd52181599ad4e3376e0021a6489d0f48
dima- <dima-tkachenko@mail.ru>
2021-03-31 02:02:13 +0300

    express js - запуск сервера, проектирование БД

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const app = express()
+const PORT = config.get('serverPort')
+
+app.use(express.json())
+app.use(express.static('static'))
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.get("dbUrl"), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => {
+            console.log(`Server started on PORT`, PORT)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
