3cdeec50279ca1c77332edfe7efc238470b6a8e5
dima- <dima-tkachenko@mail.ru>
2021-03-31 06:22:58 +0300

    add auth middleware and reg form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from "./navbar/Navbar";
 import './app.css'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Registration from "./registration/Registration";
+import Registration from "./authorization/Registration";
+import Login from "./authorization/Login";
+import {useDispatch, useSelector} from "react-redux";
+import {auth} from "../actions/user";
 
 function App() {
+    const isAuth = useSelector(state => state.user.isAuth)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(auth())
+    }, [])
+
     return (
         <BrowserRouter>
-            <div className="app">
+            <div className='app'>
                 <Navbar/>
                 <div className="wrap">
+                    {!isAuth &&
                     <Switch>
                         <Route path="/registration" component={Registration}/>
+                        <Route path="/login" component={Login}/>
                     </Switch>
+                    }
                 </div>
             </div>
         </BrowserRouter>
